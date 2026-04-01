49b8500639a64350cfafd73e2a97d3db4bb54576
Édouard Chassé <edouard.chasse@gmail.com>
2026-04-01 14:19:01 -0400

    fix: resolve false conda error and unclosed window

--- muse.bat
+++ muse.bat
@@ -1,3 +1,5 @@
+@REM Édouard Chassé
+@REM 2026-04-01
 @echo off
 
 set "ENV_NAME=muselsl"
@@ -21,6 +23,8 @@ call :try_path "%LOCALAPPDATA%\anaconda3"
 call :try_path "%LOCALAPPDATA%\Miniconda3"
 call :try_path "%LOCALAPPDATA%\miniconda3"
 
+if defined CONDA_ROOT goto :conda_ready
+
 echo Conda introuvable.
 echo.
 echo Installez Anaconda ou Miniconda, puis relancez ce script.
@@ -32,17 +36,18 @@ pause
 exit /b 1
 
 :try_path
+if defined CONDA_ROOT exit /b 0
 if exist "%~1\condabin\activate.bat" (
     echo Conda dans %~1
     set "CONDA_ROOT=%~1"
     call "%~1\condabin\activate.bat" "%~1"
-    goto :conda_ready
+    exit /b 0
 )
 if exist "%~1\Scripts\activate.bat" (
     echo Conda dans %~1
     set "CONDA_ROOT=%~1"
     call "%~1\Scripts\activate.bat" "%~1"
-    goto :conda_ready
+    exit /b 0
 )
 exit /b 0
 
@@ -91,4 +96,4 @@ if not defined CONDA_ROOT (
 ) else (
     start "muselsl" cmd /k "call %CONDA_ROOT%\condabin\activate.bat %CONDA_ROOT% && conda activate %ENV_NAME% && echo Environnement %ENV_NAME% actif. && echo Tapez 'muselsl' pour voir les commandes disponibles."
 )
-exit /b 0
+exit
